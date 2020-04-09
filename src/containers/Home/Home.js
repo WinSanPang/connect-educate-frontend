69ca26e778b49cbcb8c49174b975f6f49014b90d
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-09 15:06:52 +0100

    Create basic React app base

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>Welcome to Connect &#38; Educate</div>
+    );
+  }
+}
+
+export default Home;
+
