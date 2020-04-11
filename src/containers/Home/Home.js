cb7e312cca03d0d161af6096b39c16f9dbd31089
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-11 13:09:05 +0100

    Add basic Home Header and buttons

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { Container, Header, Image, Menu, Button } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout/Layout';
-import ButtonTest from '../../components/UI/Button/ButtonTest';
+import { HomeHeader } from '../../components/Home/HomeHeader/HomeHeader';
 
 export class Home extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
+
     return (
       <div>
         <Layout/>
-        Welcome to Connect &#38; Educate
-        <ButtonTest/>
-        
+        <HomeHeader/>
       </div>
     );
   }
