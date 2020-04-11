8fc6c37aef708b752a87a53c9ae64268a3278c4c
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-11 11:37:56 +0100

    Setup Semantic UI CSS dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 import Home from './containers/Home/Home';
 
 class App extends Component {
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import ButtonTest from '../../components/UI/Button/ButtonTest';
+
 export class Home extends Component {
   render() {
     return (
-      <div>Welcome to Connect &#38; Educate</div>
+      <div>
+        Welcome to Connect &#38; Educate
+        <ButtonTest/>
+      </div>
     );
   }
 }
