83f5058810c9115e3fc4b44fd2365f8b600ce56d
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-12 07:50:18 +0100

    Add a Not Found page

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Header, Button, Container } from 'semantic-ui-react';
 
 const NotFound = ( props ) => {
     return (
-        <div>Are you lost?</div>
+        <Container textAlign='center'>
+            <Header 
+                as='h1'
+                style={{fontSize: '50px'}}
+                >Page not Found :(</Header>
+            <Header.Subheader>Sorry - this page cannot be found. Please click <a href='/'>here</a> to go back to the homepage</Header.Subheader>
+        </Container>
     )
 };
 
