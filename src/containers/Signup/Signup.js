d655e05df493b2623f4a36a873ba757e8babbd98
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-13 17:02:38 +0100

    Change volunteer to mentor

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -10,11 +10,11 @@ const Signup = ( props ) => {
               name='handshake outline'
               size='massive'
               />
-              Volunteer with Us!
+              Become a Mentor!
               </Header.Content>
           </Header>
           <Header.Content className='Contact__Subheader'>
-              We're so pleased to hear you want to join us as a volunteer - please send us an email with some information about yourself by clicking <a href="mailto:info@maslaha.org?Subject=Conncected%20Learning%20Volunteer%20Signup%20Request">here</a>.
+              We're so pleased to hear you want to join us as a Mentor - please send us an email with some information about yourself by clicking <a href="mailto:info@maslaha.org?Subject=Conncected%20Learning%20Volunteer%20Signup%20Request">here</a>.
           </Header.Content>
           </Container>
     )
