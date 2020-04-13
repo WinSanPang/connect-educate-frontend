058657ca35c4db373fb999ab3f14fdbba81833f6
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-13 16:53:47 +0100

    Fix typo

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -14,7 +14,7 @@ const Signup = ( props ) => {
               </Header.Content>
           </Header>
           <Header.Content className='Contact__Subheader'>
-              We're so pleased to hear you want to join us as a volunteer - please send us an email with some information about yourelf by clicking <a href="mailto:info@maslaha.org?Subject=Conncected%20Learning%20Volunteer%20Signup%20Request">here</a>.
+              We're so pleased to hear you want to join us as a volunteer - please send us an email with some information about yourself by clicking <a href="mailto:info@maslaha.org?Subject=Conncected%20Learning%20Volunteer%20Signup%20Request">here</a>.
           </Header.Content>
           </Container>
     )
