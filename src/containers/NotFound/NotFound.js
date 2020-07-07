ef039da9cd5aa3e91052ea544a439b79b6be04ff
reddevilcero <reddevil_cero@hotmail.com>
2020-07-07 12:41:33 +0100

    fix all the interna link to use react router.

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Header, Container } from 'semantic-ui-react';
+import React from "react";
+import { Header, Container } from "semantic-ui-react";
 
-import './NotFound.scss';
+import "./NotFound.scss";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <Container className='NotFound'>
-            <Header 
-                as='h1'
-                className='NotFound__Header'
-                >Page not Found :(</Header>
-            <Header.Subheader className='NotFound__Paragraph'>Sorry - this page cannot be found. Please click <a href='/'>here</a> to go back to the homepage</Header.Subheader>
-        </Container>
-    )
+  return (
+    <Container className="NotFound">
+      <Header as="h1" className="NotFound__Header">
+        Page not Found :(
+      </Header>
+      <Header.Subheader className="NotFound__Paragraph">
+        Sorry - this page cannot be found. Please click <Link to="/">here</Link>{" "}
+        to go back to the homepage
+      </Header.Subheader>
+    </Container>
+  );
 };
 
 export default NotFound;
